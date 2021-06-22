1f361aa2d211f10c38d226585d960efa96a0af79
NeedforGit <48271689+NeedforGit@users.noreply.github.com>
2021-06-22 13:52:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a git clone https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/fw876/helloworld.git package/helloworld
