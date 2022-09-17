78786671e0c34db27537b177adf10ea1a9387cf6
NeedforGit <48271689+NeedforGit@users.noreply.github.com>
2022-09-17 22:13:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #git clone https://github.com/fw876/helloworld.git package/helloworld
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
