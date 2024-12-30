36e9e5b4a1f13e6232cdb9399299f7c848d9bf81
NeedforGit <48271689+NeedforGit@users.noreply.github.com>
2024-12-30 20:33:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,8 @@
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /LZ build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt /LZ build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' package/lean/default-settings/files/zzz-default-settings
+
 
 #移除不用软件包
 #rm -rf package/lean/luci-app-dockerman
